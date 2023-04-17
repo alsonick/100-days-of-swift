b030cc45c0f66a225af08ed05c39d3e881593a65
Nicholas <contactnicholas81@gmail.com>
2023-04-17 20:51:32 +0100

    Complete Day 25

--- Day24-25-26/Project4/Project4/ViewController.swift
+++ Day24-25-26/Project4/Project4/ViewController.swift
@@ -11,12 +11,15 @@ import WebKit
 class ViewController: UIViewController {
     
     var webview: WKWebView!
-    
+    var progressView: UIProgressView!
+        
     let websites: [String: UIAlertAction.Style] = [
         "apple.com": .default,
         "hackingwithswift.com": .default,
         "notnick.io": .default
     ]
+    
+    let safe = ["apple.com", "hackingwithswift.com", "notnick.io"]
     
     override func loadView() {
         webview = WKWebView()
@@ -29,7 +32,20 @@ class ViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webview, action: #selector(webview.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webview.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        let url = URL(string: "https://\(safe[0])")!
         webview.load(URLRequest(url: url))
         webview.allowsBackForwardNavigationGestures = true
     }
@@ -59,5 +75,26 @@ class ViewController: UIViewController {
 extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webview.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website.key) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
     }
 }
