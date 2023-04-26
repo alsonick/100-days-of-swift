c6675c54745aad967761fcbdf27c006f0de9e99c
Nicholas <contactnicholas81@gmail.com>
2023-04-26 13:41:03 +0100

    Complete Day 34

---
+++ Day33-34-35/Project7/Project7/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by Nicholas on 26/04/2023.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var detailItem: Petition?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <style> body { font-size: 150%; } </style>
+            </head>
+            <body>
+                \(detailItem.body)
+            </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+}
--- Day33-34-35/Project7/Project7/ViewController.swift
+++ Day33-34-35/Project7/Project7/ViewController.swift
@@ -14,13 +14,28 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Connection Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -45,6 +60,9 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
