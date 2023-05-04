5d1246cf5f5a6bba4c62d9d52ecdd40a1ae0d8b6
Nicholas <contactnicholas81@gmail.com>
2023-05-04 14:19:59 +0100

    Complte

--- Day41/Challenge/Challenge/ViewController.swift
+++ Day41/Challenge/Challenge/ViewController.swift
@@ -17,14 +17,19 @@ class ViewController: UIViewController {
     var usedLetters = [Character]()
     var words = [String]()
     var promptWord = ""
+    var tries = 0 {
+        didSet {
+            title = "Tries: \(tries)"
+        }
+    }
     var word = String()
     
     var score = 0 {
         didSet {
-            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score: \(score)", style: .plain, target: nil, action: nil)
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score: \(score)", style: .plain, target: self, action: #selector(displayScore))
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,15 +41,64 @@ class ViewController: UIViewController {
         
         nextWord()
         score = 0
-        
+        tries = word.count
     }
     
-    func check(word: String) {
-        for letter in word {
-            if promptWord.contains(letter) {
+    
+    @objc func displayScore() {
+        let ac = UIAlertController(title: "Your Score", message: "Your current score is: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func check() {
+        for letter in promptWord {
+            if word.contains(letter) {
                 usedLetters.append(letter)
             }
         }
+        
+        var updatedWordList = [String]()
+        var updatedWord = ""
+        
+        for _ in 0..<word.count {
+            updatedWordList.append("?")
+        }
+        
+        for (index, character) in usedLetters.enumerated() {
+            print(word)
+            if word.contains(character) {
+                updatedWordList[index] = String(character)
+            } else {
+                updatedWordList[index] = "?"
+            }
+        }
+        
+        for letter in updatedWordList {
+            updatedWord += letter
+        }
+        
+        // Wrong answer
+        if updatedWord == "" {
+            if score != 0 {
+                score -= 1
+            }
+            
+            let ac = UIAlertController(title: "Wrong", message: "That's incorrect.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return
+        }
+        
+        // Correct answer
+        if updatedWord == word {
+            let ac = UIAlertController(title: "Correct Answer", message: "That's correct.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            score += 1
+        }
+        
+        wordLabel.text = updatedWord
     }
     
     func nextWord() {
@@ -57,16 +111,32 @@ class ViewController: UIViewController {
         wordLabel.text = questionMarkWord
         print(word)
     }
-
-
+    
+    
     @IBAction func guessPressed(_ sender: UIButton) {
+        if tries == 0 {
+            let ac = UIAlertController(title: "Game Over", message: "You have \(tries) left. You lost.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            nextWord()
+            present(ac, animated: true)
+            tries = word.count
+            return
+        }
+        
         let ac = UIAlertController(title: "Guess Word", message: "Try guess the word.", preferredStyle: .alert)
         ac.addTextField()
         
         let guessAction = UIAlertAction(title: "Guess", style: .default) {
             [weak self, weak ac] _ in
             guard let guessedWordPrompt = ac?.textFields?[0].text else { return }
+            
+            if guessedWordPrompt == "" {
+                return
+            }
+            
             self?.promptWord = guessedWordPrompt
+            self?.check()
+            self?.tries -= 1
         }
         
         ac.addAction(guessAction)
