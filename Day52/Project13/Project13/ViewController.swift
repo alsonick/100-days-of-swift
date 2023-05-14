3cd55a20fb10b7659ee6182a7a093bff238165c5
Nicholas <contactnicholas81@gmail.com>
2023-05-14 21:45:16 +0100

    Complete Day 52

---
+++ Day52/Project13/Project13/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Nicholas on 14/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var imageView: UIImageView!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        title = "YACIFP"
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+}
+
