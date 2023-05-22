fa232a13021aa09252c8f94bc6439d95bd2e1f38
Nicholas <contactnicholas81@gmail.com>
2023-05-22 14:17:36 +0100

    Complete Day 60

---
+++ Day60-61/Project16/Project16/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Nicholas on 22/05/2023.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
