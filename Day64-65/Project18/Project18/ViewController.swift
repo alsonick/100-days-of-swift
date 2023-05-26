03c477f6aba2fea1dff02c48c66dcfb97ba89860
Nicholas <contactnicholas81@gmail.com>
2023-05-26 20:38:08 +0100

    Complete Day 64

---
+++ Day64-65/Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Nicholas on 26/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i).")
+        }
+    }
+
+
+}
+
