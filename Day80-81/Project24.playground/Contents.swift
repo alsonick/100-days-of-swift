aeefe58085768d8f5a3948a95f342e876897ee5b
Nicholas <contactnicholas81@gmail.com>
2023-06-11 14:21:51 +0100

    Complete Day 81

--- Day80-81/Project24.playground/Contents.swift
+++ Day80-81/Project24.playground/Contents.swift
@@ -75,8 +75,53 @@ let string = "This is a test string"
 //let attributedString = NSAttributedString(string: string, attributes: attributes)
 
 let attributedString = NSMutableAttributedString(string: string)
+
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        if self.contains(prefix) {
+            return self
+        }
+        
+        return "\(prefix)\(self)"
+    }
+    
+    func isNumeric() -> Bool {
+        let number = Int(self)
+        
+        if let number = number {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func lines() -> [String] {
+        var array = [String]()
+        var word = ""
+
+        for character in self {
+            if character == "\n" {
+                array.append(word)
+                word = ""
+            } else {
+                word += String(character)
+            }
+        }
+        
+        return array
+    }
+}
+
+string.withPrefix("pog")
+
+let numberString = "5"
+numberString.isNumeric()
+
+let testString = "this\nis\na\ntest\n"
+testString.lines()
