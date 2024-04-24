6ab79e44c1dc68498a1e28bc8ce6e94ffa27519d
Nicholas <contactnicholas81@gmail.com>
2024-04-24 22:49:51 +0100

    Postponed: Day 41

---
+++ Day41/Challenge/Challenge/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Challenge
+//
+//  Created by Nicholas Njoki on 24/04/2024.
+//
+
+import UIKit
+
+// TODO: Right now, i don't have the time to complete this challenge but when I do i'll come back to it later! :D
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
